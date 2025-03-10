85fc06958e7f0efde544992598f3b50f55a5bdb8
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-03-10 17:04:25 -0500

    Interfaz "IAuditable" creada

---
+++ ConsoleApp1/ConsoleApp1/IAuditable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoMundial
+{
+    interface IAuditable
+    {
+        public void AuditarTransaccion(string tipo, float cantidad) { }
+    }
+}
