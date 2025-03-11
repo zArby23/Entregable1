7b18fbd6892e050aae176706c0f373ea81ad2925
EMMANUEL <Emmaca1995@gmail.com>
2025-03-11 17:25:35 -0500

    Implementar Interface y corregir errores

--- ConsoleApp1/ConsoleApp1/CuentaCorriente.cs
+++ ConsoleApp1/ConsoleApp1/CuentaCorriente.cs
@@ -7,10 +7,23 @@ using System.Threading.Tasks;
 
 namespace BancoMundial
 {
-    public class CuentaCorriente: CuentaBancaria
+    public class CuentaCorriente: CuentaBancaria,IAuditable
     {
         public CuentaCorriente(string Direccion, string NIT, double Saldo, string Nombre, string Apellido) : base(Direccion, NIT, Saldo, Nombre, Apellido){
         }
+
+        public void AuditarTransaccion(string tipo, double cantidad) {
+
+            if (tipo.Equals("Depositar")){
+                Console.WriteLine("Cantidad depositada: " + cantidad + "$\nSaldo actual: " + this.Saldo + "$"); 
+            }
+            else if(tipo.Equals("Retirar")){
+                Console.WriteLine("Cantidad Retirada: " + cantidad + "$\nSaldo actual: " + this.Saldo + "$");
+
+            }
+
+        }
+
     }
 }
 
