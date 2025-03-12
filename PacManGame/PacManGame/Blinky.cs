510a801b62f260d1d3bf3d8b7fb9f0dec9d9c21f
Sebastian Ramírez <sebastianramirez1112697@correo.itm.edu.co>
2025-03-12 16:14:20 -0500

    Clase concreta Blinky, Clase abstracta personaje

---
+++ PacManGame/PacManGame/Blinky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManGame
+{
+    class Blinky 
+    {
+        public void Movement()
+        {
+            Console.WriteLine("Blinky se acerca directamente a PACMAN");
+        }
+    }  
+}
---
+++ PacManGame/PacManGame/personaje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacManGame
+{
+    internal class Personaje
+    {
+        String nombre;
+        double velMovimiento;
+
+        public Personaje(String nombre, double velMovimiento)
+        {
+            this.nombre = nombre;
+            this.velMovimiento = velMovimiento;
+        }
+
+        public void Mover()
+        {
+            Console.WriteLine("Moviendo personaje");
+        }
+
+        public void MostrarInformación()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Velocidad de movimiento: " + velMovimiento);
+        }
+    }
+}
